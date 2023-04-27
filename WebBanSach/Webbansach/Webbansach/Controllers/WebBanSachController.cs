36301f086e9cb63155e1c8269f9a0cdf4138847f
nguyenhoangminhtuan <119582335+nguyenhoangminhtuan@users.noreply.github.com>
2023-04-27 07:52:52 +0700

    first commit

---
+++ WebBanSach/Webbansach/Webbansach/Controllers/WebBanSachController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webbansach.Controllers
+{
+    public class WebBanSachController : Controller
+    {
+        // GET: WebBanSach
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
